fc0b8f2c698bd875dd0aae25c4e7a82ce8159ae1
Guanxun Li <victorguanxun@gmail.com>
2019-10-28 21:36:45 -0500

    Add pbmc data sets.

---
+++ R/data.R
@@ -0,0 +1,47 @@
+#' A small example version of the PBMC dataset
+#'
+#' A subsetted version of 10X Genomics' 3k PBMC dataset
+#'
+#' @format A txt file with 230 rows and 80 columns
+#' @source \url{https://github.com/satijalab/seurat/blob/master/inst/extdata/pbmc_raw.txt}
+"pbmc_raw"
+
+#' A small example version of the PBMC dataset
+#'
+#' A subsetted version of 10X Genomics' 3k PBMC dataset
+#'
+#' @format A Seurat object with the following slots filled
+#' \describe{
+#'   \item{assays}{
+#'   \itemize{Currently only contains one assay ("RNA" - scRNA-seq expression data)
+#'   \item{counts - Raw expression data}
+#'   \item{data - Normalized expression data}
+#'   \item{scale.data - Scaled expression data}
+#'   \item{var.features - names of the current features selected as variable}
+#'   \item{meta.features - Assay level metadata such as mean and variance}
+#'    }}
+#'   \item{meta.data}{Cell level metadata}
+#'   \item{active.assay}{Current default assay}
+#'   \item{active.ident}{Current default idents}
+#'   \item{graphs}{Neighbor graphs computed, currently stores the SNN}
+#'   \item{reductions}{Dimensional reductions: currently PCA and tSNE}
+#'   \item{version}{Seurat version used to create the object}
+#'   \item{commands}{Command history}
+#' }
+"pbmc_small"
+
+#' Cell type gene expression markers from GSEA data base
+#'
+#' A list contains cell types with their marker gene
+#'
+#' @format A list with 257 cell types
+#' @source \url{http://software.broadinstitute.org/gsea/msigdb/supplementary_genesets.jsp#SCSig}
+"GSEA_list"
+
+#' Cell type gene expression markers from GSEA data base
+#'
+#' A list contains cell types with their marker gene
+#'
+#' @format A list with 178 cell types
+#' @source \url{https://panglaodb.se/markers.html}
+"PanglaoDB_list"
--- R/generate_genelist.R
+++ R/generate_genelist.R
@@ -1,41 +1,38 @@
 # library(rio)
+# library(devtools)
 #
 # #### first data set
 # dta1 <- import("~/Documents/single cell/data set/GSEA/PanglaoDB_markers_17_Oct_2019.tsv")
 # celltype <- dta1$`cell type`
 # celltype <- unique(celltype)
-# mylist.names <- celltype
-# mylist <- vector("list", length(mylist.names))
-# names(mylist) <- mylist.names
+# PanglaoDB_list.names <- celltype
+# PanglaoDB_list.names <- gsub(" ", "_", PanglaoDB_list.names)
 #
-# for (i in 1:length(mylist.names)) {
+# PanglaoDB_list <- vector("list", length(PanglaoDB_list.names))
+# names(PanglaoDB_list) <- PanglaoDB_list.names
+#
+# for (i in 1:length(PanglaoDB_list.names)) {
 #   index <- which(dta1$`cell type` == celltype[i])
-#   mylist[[i]] <- dta1$`official gene symbol`[index]
+#   tmp <- dta1[, 2][index]
+#   PanglaoDB_list[[i]] <- dta1[, 2][index]
 # }
 #
-# saveRDS(mylist, file = "~/Documents/single cell/data set/GSEA/PanglaoDB_list.rds")
+# use_data(PanglaoDB_list)
 #
 # rm(list = ls())
 #
 # #### second data set
-# dta2_1 <- import("~/Documents/single cell/data set/GSEA/scsig.v1.0.metadata.xls")
-# celltype <- dta2_1$`Gene Set Standard Name`
+# dta2 <- import("~/Documents/single cell/data set/GSEA/scsig.v1.0.metadata.xls")
+# celltype <- dta2$`Gene Set Standard Name`
 # celltype <- unique(celltype)
-# mylist.names <- celltype
-# mylist <- vector("list", length(mylist.names))
-# names(mylist) <- mylist.names
+# GSEA_list.names <- celltype
+# GSEA_list <- vector("list", length(GSEA_list.names))
+# names(GSEA_list) <- GSEA_list.names
 #
-# for (i in 1:length(mylist.names)) {
-#   entry <- dta2_1[i, 12]
+# for (i in 1:length(GSEA_list.names)) {
+#   entry <- dta2[i, 12]
 #   entry <- strsplit(entry, ",")
-#   mylist[[i]] <- entry
+#   GSEA_list[[i]] <- entry[[1]]
 # }
 #
-# saveRDS(mylist, file = "~/Documents/single cell/data set/GSEA/scsig_v1.0_metadata_xls.rds")
-#
-# rm(list = ls())
-#
-# #### the third data set
-# dta2_2 <- import("~/Documents/single cell/data set/GSEA/scsig.v1.0.metadata.txt", header = FALSE)  ## same data set as dta2_1
-#
-# rm(list = ls())
+# use_data(GSEA_list)
