3ca886ca9795f412d8b06c3ce0d43559774ed3d5
Guanxun Li <victorguanxun@gmail.com>
2019-11-09 19:27:01 -0600

    Add one parameter.

--- R/getFC.R
+++ R/getFC.R
@@ -6,6 +6,7 @@
 #' @importFrom Seurat FindMarkers
 #' @param obj A Seurat object with cluster.
 #' @param min.pct only test genes that are detected in a minimum fraction of min.pct cells in either of the two populations. Meant to speed up the function by not testing genes that are very infrequently expressed.
+#' @param logfc.threshold Limit testing to genes which show, on average, at least X-fold difference (log-scale) between the two groups of cells. Increasing logfc.threshold speeds up the function, but can miss weaker signals.
 #' @param test.use Denotes which test to use. Available options are 'wilcox', 'bimod', 'roc', 'negbinom', 'poisson', 'LR', 'MAST' and 'DESeq2'.
 #'
 #' @return For each cluster, there will be a ranked gene list.
@@ -15,7 +16,7 @@
 #'                               dims = 1:10, k.param = 5, resolution = 0.75)
 #' cluster_list <- getFC(pbmc_example, min.pct = 0.25, test.use = "MAST")
 #'
-getFC <- function(obj, min.pct = 0.25, test.use = "wilcox") {
+getFC <- function(obj, min.pct = 0.1, test.use = "wilcox", logfc.threshold = 0.25) {
 
   # number of cluster
   ncluster <- length(unique(obj@meta.data$seurat_clusters))
@@ -30,7 +31,7 @@ getFC <- function(obj, min.pct = 0.25, test.use = "wilcox") {
 
     # find marker gene
     cat(paste0("Find marker genes for cluster"), i - 1, "\n")
-    cluster.markers <- Seurat::FindMarkers(obj, ident.1 = (i - 1), min.pct = min.pct, logfc.threshold = 0, test.use = test.use)
+    cluster.markers <- Seurat::FindMarkers(obj, ident.1 = (i - 1), min.pct = min.pct, logfc.threshold = logfc.threshold, test.use = test.use)
 
     # Order gene by 'avg_logFC'
     cluster.markers <- cluster.markers[order(cluster.markers[, "avg_logFC"], -cluster.markers[, "p_val_adj"], decreasing = TRUE), ]
