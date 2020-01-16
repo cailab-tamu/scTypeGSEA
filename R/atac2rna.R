681e1103d7200d4bc185982804b396baf97983e1
Guanxun Li <victorguanxun@gmail.com>
2020-01-16 11:34:06 -0600

    Rewrite atac2rna function.

--- R/atac2rna.R
+++ R/atac2rna.R
@@ -1,92 +1,107 @@
-#' Title Constructing gene activity score matrix from scATAC-seq
+#' Title Convert a peak matrix to a gene activity matrix
 #'
-#' This function constructs gene activity score matrix, which is just a sum of reads in peaks around each gene, from scATAC-seq data.
+#' This function provide two methods to create gene acticity matrix. One is to take in a peak matrix and an annotation file (gtf) and
+#' collapse the peak matrix to a gene activity matrix. And the other one is construct a feature x cell matrix from a genomic fragments file.
 #'
-#' @importFrom Seurat Read10X_h5
-#' @importFrom Seurat CreateSeuratObject
-#' @importFrom Signac SetFragments
-#' @importFrom Signac NucleosomeSignal
-#' @importFrom Signac RunTFIDF
-#' @importFrom Signac Extend
-#' @importFrom Signac FeatureMatrix
-#' @importFrom Signac GRangesToString
-#' @importFrom ensembldb genes
-#' @importFrom ensembldb seqlevelsStyle
-#' @importFrom GenomeInfoDb keepStandardChromosomes
+#' @param peaks Matrix of peak counts.
+#' @param metadata Additional cell-level metadata to add to the Seurat object. Should be a data frame where the rows are cell names and the columns are additional metadata fields.
+#' @param fragmentpath Path to tabix-indexed fragments file.
+#' @param annotation.file Path to GTF annotation file.
+#' @param qualitycontrol Whether to do quality control for peak matrix. If it is TRUE, it will compute QC Metrics and delete some cells. One needs both metadata data and fragments file to achieve quality control.
+#' @param alpha Integer value 0 or 1 to decide which method to use. alpha = 0 is to use annotation file, and alpha = 1 is to use genomic fragments file.
+#' @param seq.levels Which seqlevels to keep (corresponds to chromosomes usually).
+#' @param include.body Include the gene body?
+#' @param upstream Number of bases upstream to consider.
+#' @param downstream Number of bases downstream to consider.
+#' @param EnsDbobj For toSAF a GRangesList object. .
+#' @param chunk Number of chunks to use when processing the fragments file. Fewer chunks may enable faster processing, but will use more memory.
+#' @param filter A filter describing which results to retrieve from the database.
 #'
-#' @param countmatrixfile A path to ATAC seq data, each row of the matrix represents a region of the genome (a ‘peak’), which must be a ".h5" file.
-#' @param metadatafile A path to meta data for above matrix, which must be a ".csv" file.
-#' @param fragment.path A path to fragments file, which must be in format "tsv.gz".This represents a full list of all unique fragments across all single cells.
-#' @param mincells An integer value. When creating gene activity score matrix, only including features detected in at least this many cells.
-#' @param minfeatures An integer value. When creating gene activity score matrix, only including cells where at least this many features are detected.
-#'
-#' @return A Seurat object which contains gene activity score matrix.
+#' @return A signcle cell RNA sequence matrix.
 #' @export
 #'
-atac2rna <- function(countmatrixfile, metadatafile, fragment.path, mincells = 10, minfeatures = 1000){
+atac2rna <- function(peaks, metadata = NULL, fragmentpath = NULL, annotation.file = NULL, qualitycontrol = FALSE, alpha = 0,
+                     seq.levels = c(1:22, "X", "Y"), include.body = TRUE, upstream = 2000, downstream = 0,
+                     EnsDbobj = EnsDb.Hsapiens.v75, chunk = 50, filter = ~ gene_biotype == "protein_coding"){
+  ## do quality control
+  if (qualitycontrol = TRUE){
 
-  ## input data and set Seurat object
-  counts <- Seurat::Read10X_h5(filename = countmatrixfile)
+    if (is.null(metadata) = TRUE){
+      stop(paste("We need metadata to do quality control!"))
+    }
 
-  metadata <- read.csv(
-    file = metadatafile,
-    header = TRUE,
-    row.names = 1)
+    object <- Seurat::CreateSeuratObject(
+      counts = peaks,
+      assay = 'peaks',
+      project = 'ATAC',
+      min.cells = 1,
+      meta.data = metadata
+    )
 
-  obj <- Seurat::CreateSeuratObject(
-    counts = counts,
-    assay = 'peaks',
-    project = 'ATAC',
-    min.cells = 1,
-    meta.data = metadata
-  )
+    if (is.null(fragmentpath) = TRUE){
+      stop(paste("We need fragments file to do quality control!"))
+    }
 
-  obj <- Signac::SetFragments(
-    object = obj,
-    file = fragment.path
-  )
+    fragment.path <- fragmentpath
 
-  ## quality control
-  obj <- Signac::NucleosomeSignal(object = obj)
+    object <- SetFragments(
+      object = object,
+      file = fragment.path
+    )
+    # Computing QC Metrics
+    object <- Signac::NucleosomeSignal(object = object)
 
-  obj$pct_reads_in_peaks <- obj$peak_region_fragments / obj$total * 100
-  obj$blacklist_ratio <- obj$blacklist_region_fragments / obj$peak_region_fragments
+    object$pct_reads_in_peaks <- object$peak_region_fragments / object$total * 100
+    object$blacklist_ratio <- object$blacklist_region_fragments / object$peak_region_fragments
 
-  obj$nucleosome_group <- ifelse(obj$nucleosome_signal > 10, 'NS > 10', 'NS < 10')
+    object$nucleosome_group <- ifelse(object$nucleosome_signal > 10, 'NS > 10', 'NS < 10')
 
-  #filter out cells
-  obj <- subset(obj, subset = peak_region_fragments > 1000 & peak_region_fragments < 20000 &
-                  pct_reads_in_peaks > 15 & blacklist_ratio < 0.05 & nucleosome_signal < 10)
+    #filter out cells
+    object <- subset(object, subset = peak_region_fragments > 1000 & peak_region_fragments < 20000 & pct_reads_in_peaks > 15 & blacklist_ratio < 0.05 & nucleosome_signal < 10)
+  }
 
-  ## Normalization
-  obj <- Signac::RunTFIDF(obj)
+  ## easy method
+  if (alpha == 0){
+    if (is.null(annotation.file) == TRUE){
+      stop(paste("We need 'GTF' file for 'CreateGeneActivityMatrix' function!"))
+    }
 
-  ## Create a gene activity matrix
+    peaks <- object@assays$peaks@counts
+    activity.matrix <- Seurat::CreateGeneActivityMatrix(peak.matrix = peaks, annotation.file = annotation.file,
+                                                seq.levels = seq.levels, upstream = upstream, downstream = downstream,
+                                                include.body = include.body, verbose = FALSE)
 
-  # extract gene coordinates from Ensembl, and ensure name formatting is consistent with Seurat object
-  gene.coords <- ensembldb::genes(EnsDb.Hsapiens.v75, filter = ~ gene_biotype == "protein_coding")
-  ensembldb::seqlevelsStyle(gene.coords) <- 'UCSC'
-  genebody.coords <- GenomeInfoDb::keepStandardChromosomes(gene.coords, pruning.mode = 'coarse')
-  genebodyandpromoter.coords <- Signac::Extend(x = gene.coords, upstream = 2000, downstream = 0)
+    # return result
+    return(activity.matrix)
+  } else {
+    # Signac method, which needs fragments file.
 
-  # create a gene by cell matrix
-  gene.activities <- Signac::FeatureMatrix(
-    fragments = fragment.path,
-    features = genebodyandpromoter.coords,
-    cells = colnames(obj),
-    chunk = 10
-  )
+    if (is.null(fragmentpath) = TRUE){
+      stop(paste("We need fragments file to use 'FeatureMatrix' function."))
+    }
 
-  # convert rownames from chromsomal coordinates into gene names
-  gene.key <- genebodyandpromoter.coords$gene_name
-  names(gene.key) <- Signac::GRangesToString(grange = genebodyandpromoter.coords)
-  rownames(gene.activities) <- gene.key[rownames(gene.activities)]
+    fragment.path <- fragmentpath
 
-  # create scRNAseq Seurat object
+    # extract gene coordinates from Ensembl, and ensure name formatting is consistent with Seurat object
+    gene.coords <- ensembldb::genes(x = EnsDbobj, filter = filter)
+    seqlevelsStyle(gene.coords) <- 'UCSC'
+    genebody.coords <- GenomeInfoDb::keepStandardChromosomes(gene.coords, pruning.mode = 'coarse')
+    genebodyandpromoter.coords <- Signac::Extend(x = gene.coords, upstream = 2000, downstream = 0)
 
-  obj_return <- Seurat::CreateSeuratObject(counts = gene.activities, min.cells = mincells, min.features = minfeatures)
+    # create a gene by cell matrix
+    gene.activities <- Signac::FeatureMatrix(
+      fragments = fragment.path,
+      features = genebodyandpromoter.coords,
+      cells = colnames(object),
+      chunk = chunk
+    )
 
-  ## return gene activity score matrix
+    # convert rownames from chromsomal coordinates into gene names
+    gene.key <- genebodyandpromoter.coords$gene_name
+    names(gene.key) <- Signac::GRangesToString(grange = genebodyandpromoter.coords)
+    rownames(gene.activities) <- gene.key[rownames(gene.activities)]
 
-  return(obj_return)
+    # return result
+    return(gene.activities)
+  }
 }
