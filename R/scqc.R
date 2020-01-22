8d97754dd313685c3d90079d19f1bef6f96529fe
Guanxun Li <victorguanxun@gmail.com>
2020-01-22 14:00:14 -0600

    Add data pre-process in this function.

--- R/scqc.R
+++ R/scqc.R
@@ -1,9 +1,15 @@
-#' Single cell quality control.
+#' Single cell quality control and basic data pre-process.
 #'
 #' Check whether the input is a seurat object or not and then perform single-cell data quality control, including checking for minimum cell library size, mitochondrial ratio, outlier cells, and the fraction of cells where a gene is expressed.
+#' Ane then  the function will do basic data pre-process, which including "Normalization", "Scale data", "Find HVG" and "PCA".
 #'
 #' @importFrom Seurat CreateSeuratObject
 #' @importFrom Seurat PercentageFeatureSet
+#' @importFrom Seurat NormalizeData
+#' @importFrom Seurat FindVariableFeatures
+#' @importFrom Seurat ScaleData
+#' @importFrom Seurat RunPCA
+#' @importFrom Seurat VariableFeatures
 #' @importFrom stats sd
 #'
 #' @param obj A seurat object or a raw count gene expression.
@@ -11,14 +17,21 @@
 #' @param min.cells An integer value. Include features detected in at least this many cells.
 #' @param min.features An integer value. Include cells where at least this many features are detected.
 #' @param oversd Remove cells whose library size is greater than mean + oversd * sd. Default is null, which doesn't remove cells.
+#' @param normalization.method Method for normalization. Include 'LogNormalize', 'CLR' and 'RC'.
+#' @param scale.factor Sets the scale factor for cell-level normalization.
+#' @param selection.method How to choose top variable features. Include 'vst', 'mean.var.plot' and 'dispersion'.
+#' @param nfeatures An integer value. Define the number of features to select as top variable features.
+#' @param npcs An integer value. Define total Number of PCs to compute and store (50 by default).
 #'
-#' @return If the input is not Seurat object, it will assume it is a count gene expression matrix and transform it as a seurat object. And then do quality control to select cells. Otherwise, it will do quality control directly.
+#' @return A Seurat Object with quality control and basic data pre-process.
+#'
 #' @export
 #'
 #' @examples
-#' pbmc_example <- scqc(pbmc_small, min.cells = 1, min.features = 10)
+#' pbmc_example <- scqc(pbmc_small, min.cells = 1, min.features = 10, nfeatures = 100, npcs = 10)
 #' pbmc_example
-scqc <- function(obj, min.cells = 10, min.features = 1000, percent.mt = 5, oversd = NULL) {
+scqc <- function(obj, min.cells = 10, min.features = 1000, percent.mt = 5, oversd = NULL, normalization.method = "LogNormalize",
+                 scale.factor = 10000, selection.method = "vst", nfeatures = 2000, npcs = 50) {
   # check Seurat object
   info <- try(Seurat::Project(obj), silent = TRUE)
 
@@ -31,14 +44,27 @@ scqc <- function(obj, min.cells = 10, min.features = 1000, percent.mt = 5, overs
   obj[["percent.mt"]] <- Seurat::PercentageFeatureSet(obj, pattern = "^MT-")
   obj <- subset(obj, cells = which(obj[["percent.mt"]] < percent.mt))
 
-  if (is.null(oversd) == TRUE){
-    return(obj)
-  } else{
+  if (is.null(oversd) == FALSE){
     library_size <- obj$nCount_RNA
     sd_library_size <- sd(library_size)
     mean_library_size <- mean(library_size)
     thres_library_size <- mean_library_size + oversd * sd_library_size
     obj <- subset(obj, cells = which(obj[["nCount_RNA"]] < thres_library_size))
-    return(obj)
   }
+
+  # Normalize object
+  obj <- Seurat::NormalizeData(obj, normalization.method = normalization.method, scale.factor = scale.factor)
+
+  # Identification of highly variable features (feature selection)
+  obj <- Seurat::FindVariableFeatures(obj, selection.method = selection.method, nfeatures = nfeatures)
+
+  # Scaling the data
+  all.genes <- rownames(obj)
+  obj <- Seurat::ScaleData(obj, features = all.genes)
+
+  # Perform linear dimensional reduction
+  obj <- Seurat::RunPCA(obj, features = Seurat::VariableFeatures(object = obj), verbose = FALSE, npcs = npcs)
+
+  # return Seurat object
+  return(obj)
 }
