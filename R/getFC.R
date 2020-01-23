875e3310702b6b6815db1fc7c77f1d5e5372a01f
Guanxun Li <victorguanxun@gmail.com>
2020-01-23 09:39:15 -0600

    For other datasets, we sue Wilcoxon Rank Sum test and for scRNAseq data, we set default as 'MAST'.

--- R/getFC.R
+++ R/getFC.R
@@ -21,7 +21,7 @@
 #' pbmc_example <- doClustering(pbmc_example, dims = 1:10, k.param = 5, resolution = 0.75)
 #' cluster_list <- getFC(pbmc_example, min.pct = 0.25, test.use = "MAST")
 #'
-getFC <- function(obj, datatype = "sc", min.pct = 0.25, test.use = "wilcox", logfc.threshold = 0.1,
+getFC <- function(obj, datatype = "sc", min.pct = 0.25, test.use = "MAST", logfc.threshold = 0.1,
                   ncluster = 3) {
 
   if (datatype == "sc"){
@@ -60,20 +60,31 @@ getFC <- function(obj, datatype = "sc", min.pct = 0.25, test.use = "wilcox", log
     cluster_list <- vector("list", length(list.names))
     names(cluster_list) <- list.names
 
+    nfeatures <- nrow(obj[[1]])
+
     # Get feature list with rank for each cluster
     for (i in 1:ncluster) {
       # calculate fold-change
       index_cell <- which(cluster_membership == i)
       cell1 <- obj$data[, index_cell, drop = F]
       cell2 <- obj$data[, -index_cell, drop = F]
-      feature1 <- rowMeans(cell1)
-      feature2 <- rowMeans(cell2)
-      fold_change <- feature1/feature2
-      fold_change <- sort(fold_change, decreasing = TRUE)
+
+
+      wilcox_res <- matrix(0, nrow = nfeatures, ncol = 2)
+      colnames(wilcox_res) <- c("statistic", "p-value")
+      rownames(wilcox_res) <- rownames(obj[[1]])
+
+      for (j in 1:nfeatures){
+        test_res <- wilcox.test(cell1[j, ], cell2[j, ])
+        wilcox_res[j, 1] <- test_res$statistic
+        wilcox_res[j, 2] <- test_res$p.value
+      }
+
+      wilcox_res <- wilcox_res[order(wilcox_res[, 1], wilcox_res[, 2], decreasing = FALSE), ]
 
       # return feature list
-      cluster_list[[i]] <- fold_change
-      names(cluster_list[[i]]) <- names(fold_change)
+      cluster_list[[i]] <- wilcox_res[, 1]
+      names(cluster_list[[i]]) <- rownames(wilcox_res)
     }
     # return cluster list
     return(cluster_list)
