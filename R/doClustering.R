1c99627c6b6dccf005fddcac1273b0fab740e5fc
Guanxun Li <victorguanxun@gmail.com>
2020-01-25 15:31:45 -0600

    docluster for other type of data.

--- R/doClustering.R
+++ R/doClustering.R
@@ -6,39 +6,33 @@
 #' @importFrom Seurat Project
 #' @importFrom Seurat FindNeighbors
 #' @importFrom Seurat FindClusters
+#' @importFrom Seurat Idents
 #' @importFrom stats dist
 #' @importFrom stats hclust
 #'
-#' @param obj A Seurat object or any matrix where each column is a cell.
-#' @param datatype Data type to do cluster, which can be "sc" for single cell data and "others" for all other data type.
+#' @param obj A Seurat object.
+#' @param datatype Data type for your data, default is 'datatype = "RNA"', which is used for scRNAseq data.
 #' @param cluster_cell The cluster result for cells if it is already known.
 #' @param dims An integer value. Define dimensions of reduction to use as input. (Do cluster for single cell data.)
 #' @param k.param An integer value. Defines k for the k-nearest neighbor algorithm. (Do cluster for single cell data.)
 #' @param resolution Value of the resolution parameter, use a value above (below) 1.0 if you want to obtain a larger (smaller) number of communities. (Do cluster for single cell data.)
-#' @param method The agglomeration method to be used for hierarchical clustering, defalut is "complete".
+#' @param hclustmethod The agglomeration method to be used for hierarchical clustering, defalut is "complete".
+#' @param ncluster An integer, which is the number of cluster when your input including results from hierarchical clustering.
 #'
-#' @return For single cell type of data, it will return a Seurat object with cluster. For other type of data, it will return a list including data and result from hierarchical clustering.
+#' @return It will return a Seurat object with cluster.
 #'
 #' @export
 #'
 #' @examples
 #' pbmc_example <- scqc(pbmc_small, min.cells = 1, min.features = 10, nfeatures = 100, npcs = 10)
-#' is.null(pbmc_example@meta.data$seurat_clusters)
 #' pbmc_example <- doClustering(pbmc_example, dims = 1:10, k.param = 5, resolution = 0.75)
 #' head(pbmc_example@meta.data$seurat_clusters)
-doClustering <- function(obj, datatype = "sc", cluster_cell = NULL, dims = 1:50, k.param = 30, resolution = 0.5,
-                         method = "complete") {
-  if (datatype == "sc"){
-
+doClustering <- function(obj, datatype = "RNA", cluster_cell = NULL, dims = 1:50, k.param = 30, resolution = 0.5,
+                         hclustmethod = "complete", ncluster = 3) {
     # check Seurat object
     info <- try(Seurat::Project(obj), silent = TRUE)
     if (grepl("Error", info) == TRUE) {
-      stop(cat("The input should be Seurat Object for single cell data type.\n"))
-    }
-
-    # check data process
-    if (is.null(obj@reductions$pca) == TRUE){
-      stop(cat("The Seurat object hasn't been processed, please use 'scqc' function first.\n"))
+      stop(cat("The input should be Seurat Object.\n"))
     }
 
     # Add cluster if it has its own cluster
@@ -50,16 +44,25 @@ doClustering <- function(obj, datatype = "sc", cluster_cell = NULL, dims = 1:50,
       # return Seurat object
       return(obj)
     } else{
-      # Cluster the cells
-      obj <- Seurat::FindNeighbors(obj, dims = dims, k.param = 20)
-      obj <- Seurat::FindClusters(obj, resolution = resolution)
-      # return Seurat object
-      return(obj)
+      # for scRNAseq data
+      if (datatype == "RNA"){
+        # check data process
+        if (is.null(obj@reductions$pca) == TRUE){
+          stop(cat("The Seurat object hasn't been processed, please use 'scqc' function first.\n"))
+        }
+        # Cluster the cells
+        obj <- Seurat::FindNeighbors(obj, dims = dims, k.param = 20)
+        obj <- Seurat::FindClusters(obj, resolution = resolution)
+        # return Seurat object
+        return(obj)
+      } else{
+        # do Hierarchical Clustering
+        dta <- as.matrix(obj@assays[[1]]@counts)
+        dd <- dist(t(dta))
+        cluster_cell <- hclust(dd, method = hclustmethod)
+        cluster_membership <- cutree(cluster_cell, k = ncluster)
+        Seurat::Idents(obj) <- cluster_membership
+        return(obj)
+      }
     }
-  } else{
-    obj <- as.matrix(obj)
-    dd <- dist(t(obj))
-    cluster_cell <- hclust(dd, method = method)
-    return(list(data = obj, cluster_cell = cluster_cell))
-  }
 }
